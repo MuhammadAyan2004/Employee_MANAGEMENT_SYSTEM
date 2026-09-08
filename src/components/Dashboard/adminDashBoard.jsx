be38f33a4ba725c5fe3c67576bdf886d311942bf
Muhammad Ayan <m101ayan92@gmail.com>
2026-09-08 05:11:42 +0500

    Create adminDashBoard.jsx

---
+++ src/components/Dashboard/adminDashBoard.jsx
@@ -0,0 +1,15 @@
+import AllTask from "../others/AllTask";
+import CreateTask from "../others/CreateTask";
+import Header from "../others/Header";
+
+const AdminDashBoard = ({setUser}) => {
+  return (
+    <div className="min-h-screen w-full bg-[#1c1c1c] p-6 text-white md:p-10">
+      <Header setUser={setUser}/>
+      <CreateTask />
+      <AllTask />
+    </div>
+  );
+};
+
+export default AdminDashBoard;
