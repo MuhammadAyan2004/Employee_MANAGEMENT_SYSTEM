b2c38bdc6c3182ee4e2630dfe8924d2dab984102
Muhammad Ayan <m101ayan92@gmail.com>
2026-09-11 04:39:55 +0500

    Update adminDashBoard.jsx

--- src/components/Dashboard/adminDashBoard.jsx
+++ src/components/Dashboard/adminDashBoard.jsx
@@ -2,10 +2,10 @@ import AllTask from "../others/AllTask";
 import CreateTask from "../others/CreateTask";
 import Header from "../others/Header";
 
-const AdminDashBoard = ({setUser}) => {
+const AdminDashBoard = ({ setUser, setLoggedInUser, data }) => {
   return (
     <div className="min-h-screen w-full bg-[#1c1c1c] p-6 text-white md:p-10">
-      <Header setUser={setUser}/>
+      <Header setUser={setUser} setLoggedInUser={setLoggedInUser} data={data} />
       <CreateTask />
       <AllTask />
     </div>
