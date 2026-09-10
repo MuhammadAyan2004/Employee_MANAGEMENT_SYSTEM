7459d06c2360f5ee6c65e87c92d6b2f0f36e76df
Muhammad Ayan <m101ayan92@gmail.com>
2026-09-11 04:39:51 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./App.css";
 import Login from "./components/auth/login";
 import AdminDashBoard from "./components/Dashboard/adminDashBoard";
@@ -7,32 +7,53 @@ import { AuthContext } from "./context/Context";
 
 function App() {
   const [user, setUser] = useState(null);
+  const [loggedInUser, setLoggedInUser] = useState(null);
   const authData = useContext(AuthContext);
-  console.log(authData);
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("loggedInUser");
+    if (loggedInUser) {
+      const userData = JSON.parse(loggedInUser);
+      setUser(userData.role);
+      setLoggedInUser(userData.data);
+    }
+  }, []);
 
   const handleLogin = (email, password) => {
-    if (
-      authData &&
-      authData.admin.find((e) => e.email === email && e.password === password)
-    ) {
-      setUser("admin");
-    } else if (
-      authData &&
-      authData.employees.find(
-        (e) => e.email === email && e.password === password,
-      )
-    ) {
-      setUser("employee");
+    if (authData) {
+      const employee = authData.employees.find((e) => e.email === email && e.password === password);
+      const admin = authData.admin.find((e) => e.email === email && e.password === password);
+
+      if (employee) {
+        setUser("employee");
+        setLoggedInUser(employee);
+        localStorage.setItem("loggedInUser",JSON.stringify({ role: "employee", data:employee }));
+      }else if (admin){
+        setUser("admin");
+        setLoggedInUser(admin);
+        localStorage.setItem("loggedInUser",JSON.stringify({ role: "admin", data: admin }));
+      }
+
     } else {
       alert("Invalid Credentials");
     }
   };
   return (
     <>
-      {!user ? <Login handleLogin={handleLogin} /> : user == "admin" ? (
-        <AdminDashBoard setUser={setUser} />
+      {!user ? (
+        <Login handleLogin={handleLogin} />
+      ) : user == "admin" ? (
+        <AdminDashBoard
+          setUser={setUser}
+          setLoggedInUser={setLoggedInUser}
+          data={loggedInUser}
+        />
       ) : (
-        <EmployeeDashBoard setUser={setUser} />
+        <EmployeeDashBoard
+          setUser={setUser}
+          setLoggedInUser={setLoggedInUser}
+          data={loggedInUser}
+        />
       )}
     </>
   );
