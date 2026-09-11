68ae060a94fc3ad522516463128c0e5144579b71
Muhammad Ayan <m101ayan92@gmail.com>
2026-09-12 02:49:15 +0500

    Update newTask.jsx

---
+++ src/components/TaskList.jsx/newTask.jsx
@@ -0,0 +1,16 @@
+const NewTask = ({data}) => {
+  return (
+    <div className="flex-shrink-0 bg-amber-600 h-[100px] w-[400px] rounded-2xl p-4">
+      <div className="p-2 flex items-center justify-between">
+        {/* <button>new task</button> */}
+        <h3 className="bg-amber-800 p-2 px-5 rounded-lg font-bold ">{data.category}</h3>
+        <h4 className="mr-3 font-medium text-sm">{data.date}</h4>
+      </div>
+      <h2 className="text-2xl font-semibold mt-5">{data.title}</h2>
+      <p className="mt-3 text-sm">{data.description}</p>
+      <button className="bg-blue-400 text-white p-2 px-4">Accept</button>
+    </div>
+  );
+}
+
+export default NewTask
