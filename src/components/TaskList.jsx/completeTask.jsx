814f42eb24a7e69a5da694378b040f4f7d72de96
Muhammad Ayan <m101ayan92@gmail.com>
2026-09-12 02:49:12 +0500

    Update completeTask.jsx

---
+++ src/components/TaskList.jsx/completeTask.jsx
@@ -0,0 +1,17 @@
+const CompleteTask = ({data}) => {
+  return (
+    <div className="flex-shrink-0 bg-amber-600 min-h-[200px] w-[400px] rounded-2xl p-4">
+      <div className="p-2 flex items-center justify-between">
+        <h3 className="bg-amber-800 p-2 px-5 rounded-lg font-bold ">
+          {data.category}
+        </h3>
+        <h4 className="mr-3 font-medium text-sm">{data.date}</h4>
+      </div>
+      <h2 className="text-2xl font-semibold mt-5">{data.title}</h2>
+      <p className="mt-3 text-sm">{data.description}</p>
+      <button className="bg-blue-400 text-white p-2 px-4">completed</button>
+    </div>
+  );
+}
+
+export default CompleteTask
